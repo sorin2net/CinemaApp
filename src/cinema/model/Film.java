e5fd6b7d801ae174f737722595f8ad1191fa17d6
sorin2net <denisdumitriu95@gmail.com>
2025-08-26 19:26:56 +0300

    Create Film.java
    Adaugare clasa Film

---
+++ src/cinema/model/Film.java
@@ -0,0 +1,22 @@
+package cinema.model;
+
+import java.util.List;
+
+public class Film {
+    private String titlu;
+    private int durata; // minute
+    private String imaginePath; // calea către poza filmului
+    private List<String> ore;   // orele de difuzare
+
+    public Film(String titlu, int durata, String imaginePath, List<String> ore) {
+        this.titlu = titlu;
+        this.durata = durata;
+        this.imaginePath = imaginePath;
+        this.ore = ore;
+    }
+
+    public String getTitlu() { return titlu; }
+    public int getDurata() { return durata; }
+    public String getImaginePath() { return imaginePath; }
+    public List<String> getOre() { return ore; }
+}
